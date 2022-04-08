3737855d5a3f5fc0657a33f75d1e7845b77f9203
markthree <1801982702@qq.com>
2022-04-09 00:08:19 +0800

    chore: change client logo

--- src/client/src/components/Header.vue
+++ src/client/src/components/Header.vue
@@ -6,7 +6,7 @@ defineProps({
 	},
 	title: {
 		type: String,
-		default: 'Factory'
+		default: 'factory'
 	}
 })
 </script>
@@ -16,7 +16,11 @@ defineProps({
 		class="shadow px-2 h-15 flex items-center justify-between dark:shadow-gray-600"
 	>
 		<section class="flex items-center">
-			<img :src="logo" title="logo" class="w-9 ml-2 mr-4" />
+			<img
+				:src="logo"
+				title="logo"
+				class="w-9 ml-2 mr-4 mb-2"
+			/>
 			<h1 class="text-3xl">{{ title }}</h1>
 		</section>
 		<section class="mr-5">
